2f49dcee1f0bac52cd7136ec321fa09418279f08
Julio Rios <rios.julio.david@outlook.com>
2021-11-07 19:27:00 -0400

    schema

---
+++ src/db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
+);
+
+CREATE TABLE employee_role (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    FOREIGN KEY(role_id) REFERENCES employee_role(id)
+    ON DELETE SET NULL
+);
