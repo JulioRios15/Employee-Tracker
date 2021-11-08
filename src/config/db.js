d9eb466f9ef59457b3381ed487551a621f569a84
Julio Rios <rios.julio.david@outlook.com>
2021-11-08 18:00:53 -0400

    database connection

---
+++ src/config/db.js
@@ -0,0 +1,8 @@
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'employee_tracker_db'
+}
+
+export default config;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import mysql from 'mysql2';
+import dbConfig from './config/db.js';
+
+//Database Connection
+const connection = mysql.createConnection(dbConfig);
+
+
+
+
