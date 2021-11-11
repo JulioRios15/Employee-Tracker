bee8cd055bcbf69c881b4bcab06dd3c52e79b9af
Julio Rios <rios.julio.david@outlook.com>
2021-11-11 19:59:29 -0400

    update remove employee handler

--- src/eventHandlers/handleRemoveEmployee.ts
+++ src/eventHandlers/handleRemoveEmployee.ts
@@ -5,10 +5,10 @@ import database, {IEmployee} from '../database';
 export const handleRemoveEmployee = async (connection: Connection)  => {
 
     const employeeData:IEmployee = await inquirerPrompts.promptAllEmployees(connection);
-
-    if(employeeData == null){
-        console.log("No employees to show");      
-    }
+    console.log(employeeData);
+    
+    // return if no employees to show
+    if(!employeeData) return console.log("No employees to show");
     
     await database.remove.removeEmployee(connection, employeeData.id);
     console.log(`${employeeData.first_name} ${employeeData.last_name} has been removed`);
