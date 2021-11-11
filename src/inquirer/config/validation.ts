3f0a57a96b285c2b35eac394a66f835edf08470d
Julio Rios <rios.julio.david@outlook.com>
2021-11-10 20:45:32 -0400

    add department validation

--- src/inquirer/config/validation.ts
+++ src/inquirer/config/validation.ts
@@ -1,10 +1,24 @@
 import {Validator} from "inquirer";
+import mysql from 'mysql2';
+import dbConfig from '../../config/database';
+import database from '../../database';
 
-const validateNotEmpty: Validator = (input: string) => {
+const validateNotEmpty: Validator = async (input: string) => {
     const message = "Value cant be empty";
     return (input === "")? message : true;
 }
 
+const validateDepartmentExist: Validator = async (input: string) => {
+
+    if(input === "") return "Value cant be empty";
+
+    const departments: [] = await database.query.getDepartmentByDepartmentName(database.getConnection(), input);
+    const message = "Department exists";
+
+    return (departments.length == 0)? true : message;
+}
+
 export default {
-    validateNotEmpty
+    validateNotEmpty,
+    validateDepartmentExist
 }
