3957a07a3a50b3cfed4c5e49f4b7159d9abc175b
Julio Rios <rios.julio.david@outlook.com>
2021-11-11 20:14:56 -0400

    remove excess

--- src/inquirer/prompts/promptsAddEmployeeRole.ts
+++ src/inquirer/prompts/promptsAddEmployeeRole.ts
@@ -7,18 +7,6 @@ import validate from '../config/validation';
  * @Returns inquirer object or null if no employee roles added to database
  */
 export const promptAddEmployeeRole = async (connection: Connection) => {
-    // Get the employee Roles from our sql database
-    const employeeRoles: [] = await database.query.getALlEmployeeRoles(connection);
-
-    //Return null if no employee roles added to the database
-    if(employeeRoles.length == 0) return null;
-
-    // Create an Array to Store all roles added in our database
-    let rolesArr: string[] = [];
-
-    // Store all roles in rolesArr
-    employeeRoles.forEach((item: any) => rolesArr.push(item.title));
-
 
     const roleTitleQuestion: Question = {
         type: "input",
