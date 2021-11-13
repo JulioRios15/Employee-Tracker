1f2fac17a6c5f3eb472456ac129c7e728ed77049
Julio Rios <rios.julio.david@outlook.com>
2021-11-12 21:49:49 -0400

    update handle add department

--- src/eventHandlers/handleAddDepartment.ts
+++ src/eventHandlers/handleAddDepartment.ts
@@ -1,13 +1,21 @@
 import { Connection } from "mysql2";
 import inquirerPrompts from '../inquirer/prompts';
+import {promptConfirm, IConfirmAnswer} from '../inquirer/prompts/promptConfirm';
 import database from '../database';
 
 
 
 export const handleAddDepartment = async (connection: Connection) => {
     
-    const departmentData = await inquirerPrompts.promptAddDepartment();
-    const departmentName = departmentData.departmentName;
+    let addMoreDepartments: IConfirmAnswer = {"confirm": true};
 
-    await database.insert.addDepartment(connection, departmentName);   
+    while (addMoreDepartments.confirm) {
+        const departmentData = await inquirerPrompts.promptAddDepartment();
+        const departmentName = departmentData.departmentName;
+    
+        await database.insert.addDepartment(connection, departmentName);
+
+        addMoreDepartments = await promptConfirm("Do you want to add another department?")
+    }
+ 
 }
