ec516c723a9bb45b0dd1abc6a0af2bd6a1ff0a7e
Julio Rios <rios.julio.david@outlook.com>
2021-11-12 22:40:07 -0400

    update remove employee

--- src/eventHandlers/handleRemoveEmployee.ts
+++ src/eventHandlers/handleRemoveEmployee.ts
@@ -1,14 +1,26 @@
 import {Connection} from 'mysql2';
 import inquirerPrompts from '../inquirer/prompts';
 import database, {IEmployee} from '../database';
+import {promptConfirm, IConfirmAnswer} from '../inquirer/prompts/promptConfirm';
 
 export const handleRemoveEmployee = async (connection: Connection)  => {
+    
+    let removeMoreEmployees: IConfirmAnswer = {"confirm": true};
 
-    const employeeData:IEmployee = await inquirerPrompts.promptAllEmployees(connection);
+    while (removeMoreEmployees.confirm) {
+
+        //Prompts a list of all employees
+        const employeeData:IEmployee = await inquirerPrompts.promptAllEmployees(connection);
     
-    // return if no employees to show
-    if(!employeeData) return console.log("No employees to show");
-    
-    await database.remove.removeEmployee(connection, employeeData.id);
-    console.log(`${employeeData.first_name} ${employeeData.last_name} has been removed`);
+        // return if no employees to show
+        if(!employeeData) return console.log("---------No employees to show---------");
+        
+        //Remove employee and log employee removed
+        await database.remove.removeEmployee(connection, employeeData.id);
+        console.log(`${employeeData.first_name} ${employeeData.last_name} has been removed`);
+
+        //Ask the user to remove another employee
+        removeMoreEmployees = await promptConfirm("Do you want to remove another employee?")  
+    }
+
 }
